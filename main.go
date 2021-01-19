3a830bcc055773d635608e43b6048aeb5d43b2aa
Fauzan Maulana <fauzanfm735@gmail.com>
2021-01-19 09:28:22 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var run bool = true
+
+func getInput(text string) string {
+	fmt.Print(text + ": ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	value := strings.Trim(input, "\n")
+	return value
+}
+
+func inputText(input string, text func(string) string) string {
+	output := text(input)
+	return output
+}
+
+func printText(texts ...string) {
+	for _, text := range texts {
+		fmt.Println(text)
+	}
+}
+
+func main() {
+	// define slice
+	var text = []string{"aji", "anggar", "bimo", "rifat", "faris"}
+	var newText []string
+
+	for run {
+		input := inputText("please input some name", getInput)
+
+		// append new slice
+		newText = append(text, input)
+		text = newText
+
+		// define output
+		printText(text...)
+
+		confirm := inputText("want more? (y/n)", getInput)
+		if confirm == "n" {
+			run = false
+		}
+	}
+
+	fmt.Println("program finish")
+}
